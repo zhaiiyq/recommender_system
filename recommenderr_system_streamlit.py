647ade830b1963b12295490aed6e03dae2b59c91
zhaiiyq <akzajykp@gmail.com>
2025-05-07 14:46:11 +0500

    Update recommenderr_system_streamlit.py

--- recommenderr_system_streamlit.py
+++ recommenderr_system_streamlit.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Загружаем данные
-@st.cache_data
+@st.cache
 def load_data():
     # Замените на ваш путь к датасету
     df = pd.read_csv('games1.csv')
